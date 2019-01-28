f501b52b50e2d8331a65cac354b72f5c8b5ef3ea
remi_bourgarel <bourgarel.remi@gmail.com>
2019-01-28 22:13:05 +0100

    Config for CI tests

--- RemiBou.CosmosDB.Migration.Tests/CosmosDBMigrationTests.cs
+++ RemiBou.CosmosDB.Migration.Tests/CosmosDBMigrationTests.cs
@@ -13,8 +13,9 @@ namespace RemiBou.CosmosDB.Migration.Tests
 
         public CosmosDBMigrationTests()
         {
+            
             documentClient = new DocumentClient(
-             new Uri("https://localhost:8081"),
+             new Uri(Environment.GetEnvironmentVariable("CosmosDBEndpoint") ?? "https://localhost:8081"),
              "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
 
         }
